fcc8040d19c06c50b3060f986debb9c8417060bc
James Aules <james_aules@icloud.com>
2023-02-27 17:05:53 -0500

    adding npm spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import './App.css';
+import Loader from 'react-loader-spinner';
 // import AboutUs from './components/AboutUs'
 // import Packages from './components/Packages'
 
@@ -13,7 +14,14 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-          <Suspense fallback={<h1>Loading...</h1>} >
+          <Suspense fallback={<Loader
+                                  type='Bars'
+                                  color='#FF00FF'
+                                  height={200}
+                                  width={200}
+                                  style={{margin: '0 auto'}}
+                                  />} >
+              <h1>Hello World!</h1>
             <AboutUs />
             <Packages />
           </Suspense>
